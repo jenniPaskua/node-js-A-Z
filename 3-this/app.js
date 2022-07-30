23dbccf590d49fee6f1dc763a80c061c85e69e08
조지은 <jenniPaskua@gmail.com>
2022-07-30 21:09:22 +0900

    4.5 this란 (브라우저와의 차이점)

---
+++ 3-this/app.js
@@ -0,0 +1,25 @@
+function hello() {
+  console.log(this);
+  console.log(this === global);
+}
+
+hello();
+
+class A {
+  constructor(num) {
+    this.num = num;
+  }
+  memberFunction() {
+    console.log("-----class-----");
+    console.log(this);
+    //클래스에서의 this는 global이 아니다! 클래스 자신이다!
+    console.log(this === global);
+  }
+}
+
+const a = new A(1);
+a.memberFunction();
+
+console.log("----global scope----");
+console.log(this);
+console.log(this === module.exports);
