3a016d5cabc9dc0b6923ab08baca8f89e7b80f94
조지은 <jenniPaskua@gmail.com>
2022-07-30 21:04:37 +0900

    4.4 콘솔 로그의 진실

--- 1-global/app.js
+++ 1-global/app.js
@@ -8,3 +8,51 @@ global.hello = () => {
 global.hello();
 hello();
 // https://academy.dream-coding.com/courses/take/node/lessons/24539336-4-3
+
+console.log("log"); // 개발
+console.info("info"); // 정보
+console.warn("warn"); // 경보
+console.error("error"); // 에러, 사용자 에러, 시스템 에러
+
+// assert
+console.assert(2 === 3, "not same!");
+console.assert(2 === 2, "same!");
+
+// print object
+const student = {name: "ellie", age: 20, company: {name: "AC"}};
+console.log(student);
+console.table(student);
+console.dir(student, {showHidden: true, colors: false, depth: 0});
+
+// measuring time
+console.time("for loop");
+for (let i = 0; i < 10; i++) {
+  i++;
+}
+console.timeEnd("for loop"); // 성능 체크 시 필요!
+
+// counting
+function a() {
+  console.count("a function");
+}
+
+a();
+console.countReset("a function");
+a();
+
+// trace
+
+function f1() {
+  f2();
+}
+
+function f2() {
+  f3();
+}
+
+function f3() {
+  console.log("f3");
+  console.trace();
+}
+
+f1();
