8b64dde12c6f13ca8c9e82b3921ed2047dfc42ff
조지은 <jenniPaskua@gmail.com>
2022-08-01 19:37:02 +0900

    4,13 file - 다양한 사용법

---
+++ 10-file/app2.js
@@ -0,0 +1,22 @@
+const fs = require("fs").promises;
+
+// read a file
+fs.readFile("./file-text.txt")
+  .then((data) => console.log("data:", data))
+  .catch(console.error);
+
+// writing a file
+fs.writeFile("./file-text.txt", "Hello, Jenn Jenn!").catch(console.error);
+
+// append a file
+fs.appendFile("./file-text.txt", "yo, Jenn Jenn!")
+  .then(() => {
+    //copy
+    fs.copyFile("./file-text.txt", "./file2.txt").catch(console.error);
+  })
+  .catch(console.error);
+
+// folder
+fs.mkdir("sub-folder").catch(console.error);
+
+fs.readdir("./").then(console.log).catch(console.error);
