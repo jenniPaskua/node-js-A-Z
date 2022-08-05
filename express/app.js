2f256175dad8d6e17754b6500546013d428029b4
조지은 <jenniPaskua@gmail.com>
2022-08-05 10:23:47 +0900

    11.4 요청에 대해서

---
+++ express/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+app.get("/sky/:id", (req, res, next) => {
+  console.log(req.params);
+  console.log(req.params.id);
+  console.log(req.query);
+  console.log(req.query.keyword);
+
+  res.send("hi!");
+});
+app.listen(8080);
+// port IP주소
