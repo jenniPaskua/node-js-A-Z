a9d1951eed66b9b2f5d340669cc33b715127fa20
조지은 <jenniPaskua@gmail.com>
2022-09-30 13:22:25 +0900

    11.6 미들웨어의 중요한 특징

--- express/app.js
+++ express/app.js
@@ -1,16 +1,42 @@
 import express from "express";
 const app = express();
 
-app.get("/sky/:id", (req, res, next) => {
-  console.log(req.params);
-  console.log(req.params.id);
-  console.log(req.query);
-  console.log(req.query.keyword);
+app.all("/api", (req, res, next) => {
+  console.log("all");
+  next();
+});
 
-  res.send("hi!");
+app.use("/sky", (req, res, next) => {
+  console.log("use");
+  next();
 });
+
+// middleware에 대하여
+// 설정된 순서가 중요함 - next()
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("first");
+    next(new Error("error"));
+  },
+  (req, res, next) => {
+    console.log("first2");
+    next();
+  }
+);
+
 app.get("/", (req, res, next) => {
-  console.log("get");
+  console.log("second");
 });
+
+app.use((req, res, next) => {
+  res.status(404).send("Not available!");
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send("Sorry, try later!");
+});
+
 app.listen(8080);
 // port IP주소
